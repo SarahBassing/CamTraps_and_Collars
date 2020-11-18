79f076605daf9308dc9949ec919a35b47d766c77
SarahBassing <sarah.bassing@gmail.com>
2020-11-17 16:36:04 -0800

    Added filter to remove extra fine-scale locations & changed DOP filtering

--- Scripts/Collar_DataCleaning.R
+++ Scripts/Collar_DataCleaning.R
@@ -21,7 +21,7 @@
   ##  mule deer location data for the WPPP. I have expanded it.
   ##  =========================================================
 
-  #  Clean workspace and install libraries
+  #  Clean work space and load libraries
   rm(list = ls())
   
   library(lubridate)
@@ -38,23 +38,24 @@
   ####  ====================================================
   ####  Read in Capture, Mortality, and Telemetry data  ####
   
-  #  Capture data (latest download: 10.25.20)
-  md_cap <- read.csv("./Data/Capture (MD) 10.25.20.csv", stringsAsFactors = FALSE) %>%
+  #  Capture data (latest download: 11.16.20)
+  #  Note the "ï.."
+  md_cap <- read.csv("./Data/Capture (MD)_11.16.20.csv", stringsAsFactors = FALSE) %>%
     mutate(IndividualIdentifier = ï..IndividualIdentifier) %>%
     select(-ï..IndividualIdentifier)
-  elk_cap <- read.csv("./Data/Capture (Elk) 10.25.20.csv", stringsAsFactors = FALSE) %>%
+  elk_cap <- read.csv("./Data/Capture (Elk)_11.16.20.csv", stringsAsFactors = FALSE) %>%
     mutate(IndividualIdentifier = ï..IndividualIdentifier) %>%
     select(-ï..IndividualIdentifier)
-  wtd_cap <- read.csv("./Data/Capture (WTD) 10.25.20.csv", stringsAsFactors = FALSE) %>%
+  wtd_cap <- read.csv("./Data/Capture (WTD)_11.16.20.csv", stringsAsFactors = FALSE) %>%
     mutate(IndividualIdentifier = ï..IndividualIdentifier) %>%
     select(-ï..IndividualIdentifier) 
   
   #str(md_cap)#; head(md_cap)
   
-  #  Mortality data (latest download: 10.25.20)
-  md_mort <- read.csv("./Data/Mortality (MD) 10.25.20.csv", stringsAsFactors = FALSE) 
-  elk_mort <- read.csv("./Data/Mortality (Elk) 10.25.20.csv", stringsAsFactors = FALSE) 
-  wtd_mort <- read.csv("./Data/Mortality (WTD) 10.25.20.csv", stringsAsFactors = FALSE) 
+  #  Mortality data (latest download: 11.16.20)
+  md_mort <- read.csv("./Data/Mortality (MD)_11.16.20.csv", stringsAsFactors = FALSE) 
+  elk_mort <- read.csv("./Data/Mortality (Elk)_11.16.20.csv", stringsAsFactors = FALSE) 
+  wtd_mort <- read.csv("./Data/Mortality (WTD)_11.16.20.csv", stringsAsFactors = FALSE) 
   
   #str(md_mort)#; head(md_mort)
   
@@ -68,25 +69,25 @@
   #  Add column that converts times to UTC
   #  Add column that converts times to Pacific Standard Time (add 8 hours), ignoring daylight savings
   #  Add column that floors the time to the nearest hour not ahead
-  md_tel <- read.csv("./Data/dev_telem_md_10.25.20.csv") %>%    
+  md_tel <- read.csv("./Data/dev_telem_md_11.16.20.csv") %>%    
     mutate(daytime = mdy_hms(ObservationDateTimePST, tz = "America/Los_Angeles"),
            UTCdt = with_tz(daytime, "UTC"),
            Finaldt = with_tz(UTCdt, tzone = "Etc/GMT+8"),
            Floordt = floor_date(Finaldt, unit = "hour")) %>%
     select(-InWashingtonJurisdiction)
-  elk_tel <- read.csv("./Data/dev_telem_elk_10.25.20.csv") %>%  
+  elk_tel <- read.csv("./Data/dev_telem_elk_11.16.20.csv") %>%  
     mutate(daytime = mdy_hms(ObservationDateTimePST, tz = "America/Los_Angeles"),
            UTCdt = with_tz(daytime, "UTC"),
            Finaldt = with_tz(UTCdt, tzone = "Etc/GMT+8"),
            Floordt = floor_date(Finaldt, unit = "hour")) %>%
     select(-InWashingtonJurisdiction)
-  wtd_tel <- read.csv("./Data/dev_telem_wtd_10.25.20.csv") %>%
+  wtd_tel <- read.csv("./Data/dev_telem_wtd_11.16.20.csv") %>%
     mutate(daytime = mdy_hms(ObservationDateTimePST, tz = "America/Los_Angeles"),
            UTCdt = with_tz(daytime, "UTC"),
            Finaldt = with_tz(UTCdt, tzone = "Etc/GMT+8"),
            Floordt = floor_date(Finaldt, unit = "hour")) %>%
     select(-InWashingtonJurisdiction)
-  no_fix <- read.csv("./Data/dev_telem_vec_nofix_10.25.20.csv") %>%
+  no_fix <- read.csv("./Data/dev_telem_vec_nofix_11.16.20.csv") %>%
     #  Add extra columns to match the telemetry data with successful fixes
     mutate(Latitude = "NA",
            Longitude = "NA",
@@ -97,12 +98,12 @@
            UTCdt = with_tz(daytime, "UTC"),
            Finaldt = with_tz(UTCdt, tzone = "Etc/GMT+8"),
            Floordt = floor_date(Finaldt, unit = "hour")) %>%
-    select(-OBJECTID) %>%
+    #select(-OBJECTID) %>%
     #  Reorganize columns to match column order in telemetry data
     relocate(c("Latitude", "Longitude"), .after = CollarID) %>%
     relocate("ValidLocation", .after = DbLoadedDateTimePST) %>%
     relocate(c("VEC_DOP", "VEC_Height"), .after = VEC_Origin)
-  colnames(no_fix)[1] <- "OBJECTID"
+  #colnames(no_fix)[1] <- "OBJECTID"
   
   #  Extract species-specific missing fixes
   md_nofix <- no_fix[no_fix$Species == "Mule Deer",]
@@ -181,20 +182,22 @@
   elk_info <- spp_info(elk_cap, elk_mort, elk_capmort, elk_tel)  
   wtd_info <- spp_info(wtd_cap, wtd_mort, wtd_capmort, wtd_tel)  
   
-  #  Remove this one rude mule deer that for some reason ruins the function... bad coding practice, I know!
-  #  89MD18 and R89ND18 have the same collar ID b/c 89MD18 died 4 days after capture
-  # md_info <- md_info[md_info$IndividualIdentifier != "89MD18",]   # ... seems to be working now
+  #  Save for later use
+  # write.csv(md_info, paste0('md_info ', Sys.Date(), '.csv'))
+  # write.csv(elk_info, paste0('elk_info ', Sys.Date(), '.csv'))
+  # write.csv(wtd_info, paste0('wtd_info ', Sys.Date(), '.csv'))
   
-
   ####  Running list of collars that got nixed in this stage  ####
   #  Mule deer
-  #  3969MD17, 3958MD17, 89MD18
+  #  3969MD17 (male), 3958MD17 (capture-related mortality)
   
   #  Elk
-  #  4836ELK20
+  #  4836ELK20 (male)
   
   #  White-tailed deer
-  #  019WTD17, 023WTD17, 70WTD18, 85WTD19, 90WTD19
+  #  019WTD17 (capture-related mortality), 023WTD17 (collar never transmitted), 
+  #  70WTD18 (capture-related mortality), 85WTD19 (collar never transmitted), 
+  #  90WTD19 (collar never transmitted)
   
   ####  =====================================================
   ####  Create MASTER data set for each ungulate species ####
@@ -302,30 +305,32 @@
   #  Drop missing fixes and locations with poor accuracy
   #  Drop extra locations when fix schedule increases
   
-  #  Drop missing fixes & locations with poor accuracy for a cleaner data set
-  #  DOP >10 is not good (~20-30 meter accuracy)
+  #  Drop missing fixes 
+  #  Drop locations with poor accuracy for a cleaner data set
+  #  No 2D locations and no locations with VEC_Height <0 or >2000 (arbitrary cutoffs)
+  #  DOP >10 is not good (~20-30 meter accuracy)--- using DOP = 8 as cutoff
   md_clean <- md_master %>%
     filter(VEC_FixType != "No fix") %>%
-    #filter(VEC_FixType != "GPS-2D") %>%
+    filter(VEC_FixType != "GPS-2D") %>%
     #filter(VEC_Height < 2000 & VEC_Height > 0) %>%
-    filter(VEC_DOP <= 10)
+    filter(VEC_DOP <= 8)
   elk_clean <- elk_master %>%
     filter(VEC_FixType != "No fix") %>%
-    #filter(VEC_FixType != "GPS-2D") %>%
-    filter(VEC_Height < 2000 & VEC_Height > 0) %>%
-    filter(VEC_DOP <= 10)
+    filter(VEC_FixType != "GPS-2D") %>%
+    #filter(VEC_Height < 2000 & VEC_Height > 0) %>%
+    filter(VEC_DOP <= 8)
   wtd_clean <- wtd_master %>%
     filter(VEC_FixType != "No fix") %>%
-    #filter(VEC_FixType != "GPS-2D") %>%
+    filter(VEC_FixType != "GPS-2D") %>%
     #filter(VEC_Height < 2000 & VEC_Height > 0) %>%
-    filter(VEC_DOP <= 10) 
+    filter(VEC_DOP <= 8) 
   
-  #  VEH_Height also associated with accuracy so need to see if there are any 
+  #  VEH_Height also associated with accuracy so need to see if there are any
   #  odd outliers that should be excluded due to low accuracy fixes
-  hist(elk_clean$VEC_Height, breaks = c(100), main = "Elk locations (ALL VEC_Height values)")
-  plot(elk_clean$VEC_Height)  # takes awhile
-  summary(elk_clean$VEC_Height)
-  oddball <- elk_clean[elk_clean$VEC_Height >= 2000 | elk_clean$VEC_Height < 0,]
+  # hist(elk_clean$VEC_Height, breaks = c(100), main = "Elk locations (ALL VEC_Height values)")
+  # plot(elk_clean$VEC_Height)  # takes awhile
+  # summary(elk_clean$VEC_Height)
+  # oddball <- elk_clean[elk_clean$VEC_Height >= 2000 | elk_clean$VEC_Height < 0,]
 
   
   #  Save locations with high accuracy fixes only
@@ -358,7 +363,13 @@
       mutate(
         Latitude = as.numeric(Latitude),
         Longitude = as.numeric(Longitude)
-      ) 
+      ) %>%
+      #  Thin data to retain only the 1st location on the hour
+      #  Important when collar goes into mortality mode but animal is still alive- 
+      #  Fix rate increases but flooring process puts all those times on the hour
+      group_by(ID) %>%
+      distinct(Floordt, .keep_all = TRUE) %>%   # .keep_all = TRUE saves all columns
+      ungroup()
     return(skinny)
   }
   
@@ -398,25 +409,25 @@
   # write.csv(elk_skinny, paste0('elk_skinny ', Sys.Date(), '.csv'))
   # write.csv(wtd_skinny, paste0('wtd_skinny ', Sys.Date(), '.csv'))
   
- 
+  
   ####  ============================================
   ####  Review individual collars for oddities  ####
   
   #  Plot all locations for a given species and look at their distribution
   #  Plot all locations for a given individual and look at their distribution
   #  Take special note of individuals with odd locations or distributions
-  
+
   #  Load required packages for spatial work
   require(sf)
   require(ggplot2)
-  
+
   #  Read in study area shapefiles and reproject to lat/long
   OK_SA <- st_read("./Shapefiles/fwdstudyareamaps", layer = "METHOW_SA")
   NE_SA <- st_read("./Shapefiles/fwdstudyareamaps", layer = "NE_SA")
   wgs84 <- st_crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
   OK_wgs84 <- st_transform(OK_SA, wgs84)
   NE_wgs84 <- st_transform(NE_SA, wgs84)
-  
+
   #  Make collar location data spatial
   md_spdf <- st_as_sf(md_skinny, coords = c("Longitude", "Latitude"), crs = wgs84)
   elk_spdf <- st_as_sf(elk_skinny, coords = c("Longitude", "Latitude"), crs = wgs84)
@@ -435,9 +446,9 @@
   #  Plot one animal
   ggplot() +
     geom_sf(data = NE_SA, fill = NA) +
-    geom_sf(data = ind_animal[[1]], aes(color = ID))  
-  
-  
+    geom_sf(data = ind_animal[[1]], aes(color = ID))
+
+
   #  Plotting by study area with study area boundary for context
   #  Function to plot locations from individual animals in the NORTHEAST study area
   plot_telem_NE <- function(spdf){
@@ -460,7 +471,7 @@
     }
     return(plot_list)
   }
-  
+
   #  Function to plot locations from individual animals in the OKANOGAN study area
   plot_telem_OK <- function(spdf){
     #  Split out spatial points df by individual animal ID
@@ -482,21 +493,21 @@
     }
     return(plot_list)
   }
-  
+
   #  Feed elk & wtd through function to map individual telemetry data in NE
   #  Plot an example map
   elk_NE_maps <- plot_telem_NE(elk_spdf)
   plot(elk_NE_maps[[1]])
-  
+
   wtd_NE_maps <- plot_telem_NE(wtd_spdf)
   plot(wtd_NE_maps[[1]])
-  
+
   #  Feed mule deer through function to map individual telemetry data in OK
   #  Plot an example map
   md_OK_maps <- plot_telem_OK(md_spdf)
   plot(md_OK_maps[[1]])
-  
-  
+
+
   #  Plotting zoomed in location data without study area boundary for context
   plot_telem <- function(spdf){
     #  Split out spatial points df by individual animal ID
@@ -509,7 +520,7 @@
     for(i in 1:length(unique(ind_animal))) {
       names <- c(names, unique(as.character(ind_animal[[i]]$ID)))
       plot <- ggplot() +
-        geom_sf(data = ind_animal[[i]], aes(color = Floordt)) + 
+        geom_sf(data = ind_animal[[i]], aes(color = Floordt)) +
         labs(title = paste(names[i], "Locations", sep = " "), x = "Longitude",
              y = "Latitude") +
         #  Keep the legend but drop the title
@@ -519,48 +530,48 @@
     }
     return(plot_list)
   }
-  
-  #  Feed all species through function to map zoomed in telemetry data 
+
+  #  Feed all species through function to map zoomed in telemetry data
   elk_maps <- plot_telem(elk_spdf)
   wtd_maps <- plot_telem(wtd_spdf)
   md_maps <- plot_telem(md_spdf)
-  
+
   print(elk_maps[[1]])
-  
+
   #  Save individual plots in a single pdf for each species
   #  With NE or OK study area boundary for context
-  pdf("./Outputs/elk_NE_maps2.pdf")
-  for (i in 1:length(unique(elk_NE_maps))) {
-    print(elk_NE_maps[[i]])
-  }
-  dev.off()
-  pdf("./Outputs/wtd_NE_maps.pdf")
-  for (i in 1:length(unique(wtd_NE_maps))) {
-    print(wtd_NE_maps[[i]])
-  }
-  dev.off()
-  pdf("./Outputs/md_OK_maps.pdf")
-  for (i in 1:length(unique(md_OK_maps))) {
-    print(md_OK_maps[[i]])
-  }
-  dev.off()
-  
-  #  Without study area boundary for context
-  pdf("./Outputs/elk_maps2.pdf")
-  for (i in 1:length(unique(elk_maps))) {
-    print(elk_maps[[i]])
-  }
-  dev.off()
-  pdf("./Outputs/wtd_maps.pdf")
-  for (i in 1:length(unique(wtd_maps))) {
-    print(wtd_maps[[i]])
-  }
-  dev.off()
-  pdf("./Outputs/md_maps.pdf")
-  for (i in 1:length(unique(md_maps))) {
-    print(md_maps[[i]])
-  }
-  dev.off()
+  # pdf("./Outputs/elk_NE_maps2.pdf")
+  # for (i in 1:length(unique(elk_NE_maps))) {
+  #   print(elk_NE_maps[[i]])
+  # }
+  # dev.off()
+  # pdf("./Outputs/wtd_NE_maps2.pdf")
+  # for (i in 1:length(unique(wtd_NE_maps))) {
+  #   print(wtd_NE_maps[[i]])
+  # }
+  # dev.off()
+  # pdf("./Outputs/md_OK_maps2.pdf")
+  # for (i in 1:length(unique(md_OK_maps))) {
+  #   print(md_OK_maps[[i]])
+  # }
+  # dev.off()
+  #
+  # #  Without study area boundary for context
+  # pdf("./Outputs/elk_maps2.pdf")
+  # for (i in 1:length(unique(elk_maps))) {
+  #   print(elk_maps[[i]])
+  # }
+  # dev.off()
+  # pdf("./Outputs/wtd_maps.pdf")
+  # for (i in 1:length(unique(wtd_maps))) {
+  #   print(wtd_maps[[i]])
+  # }
+  # dev.off()
+  # pdf("./Outputs/md_maps.pdf")
+  # for (i in 1:length(unique(md_maps))) {
+  #   print(md_maps[[i]])
+  # }
+  # dev.off()
   
   #  NOTES
   #  Many mule deer collars still include at least one start point in Winthrop (hotel?)
@@ -612,30 +623,30 @@
     missed <- nrow(nofix)
     #  Percent of missed fixes out of total possible locations
     perc_missed <- nrow(nofix)/nrow(master)
-    
+
     #  Total number of successful fixes
     locations <- nrow(clean)
     #  Number of low accuracy fixes (DOP > 10)
     lowacc <- nrow(filter(master, VEC_DOP > 10))
     #  Percent of low accuracy fixes out of all successful locations
     perc_lowacc <- lowacc/nrow(clean)
-    
+
     #  Combine all summary stats
     stats <- c(total, missed, perc_missed, locations, lowacc, perc_lowacc)
-    
+
     return(stats)
-    
+
   }
-  
+
   #  Run species location data through summary stats function
   md_stats <- fix_stats(md_master, md_nofix, md_clean)
   elk_stats <- fix_stats(elk_master, elk_nofix, elk_clean)
   wtd_stats <- fix_stats(wtd_master, wtd_nofix, wtd_clean)
-  
+
   telem_stats <- as.data.frame(rbind(md_stats, elk_stats, wtd_stats))
   colnames(telem_stats) <- c("Total_Locs", "Missed_Locs", "Perc_Missed", "Successful_Locs", "LowAccuracy_Locs", "Perc_LowAccuracy")
   print(telem_stats)
-  
+
   #  Collar-specific issues
   #  Function to calculate missing data and low accuracy fixes per animal
   lame_locs <- function(master) {
@@ -646,7 +657,7 @@
       #  Spread data for easier summarizing below (wide format)
       spread(ID, n)
     colnames(NoFix)[1] <- "Problem"
-    
+
     InaccFix <- master %>%
       group_by(ID) %>%
       na.omit() %>%
@@ -655,7 +666,7 @@
       #  Spread data for easier summarizing below (wide format)
       spread(ID, n)
     colnames(InaccFix)[1] <- "Problem"
-    
+
     Collar_probs <- rbind(NoFix, InaccFix)
     Problem_Type <- c("Missed Fix", "Missed Fix", "Low Accuracy", "Low Accuracy")
     Collar_probs <- cbind(Problem_Type, Collar_probs)
@@ -668,11 +679,11 @@
         Problem_Type = Problem_Type,
         Problem = Problem
       )
-    
+
     return(Collar_problems)
-    
+
   }
-  
+
   #  Run species-specific data through function and combine
   md_probs <- lame_locs(md_master) %>%
     mutate(Species = "Mule Deer")
@@ -682,10 +693,10 @@
     mutate(Species = "White-tailed Deer")
   Problem_locs <- rbind(md_probs, elk_probs, wtd_probs) %>%
     relocate(Species)  # default puts this column first
-  
+
   #  Save summary stats
-  write.csv(telem_stats, paste0('Telemetry_stats ', Sys.Date(), '.csv'))
-  write.csv(Problem_locs, paste0('Collar_stats ', Sys.Date(), '.csv'))
+  # write.csv(telem_stats, paste0('Telemetry_stats ', Sys.Date(), '.csv'))
+  # write.csv(Problem_locs, paste0('Collar_stats ', Sys.Date(), '.csv'))
   
   
   #  Fin
